2161eafbb387987b6b5c845d3fabee2c6fc1ffe1
Randy Pangestu Kuswana <randy.kuswana@vida.id>
2024-03-20 04:34:10 +0700

    Finalize v0.1 version

--- app.py
+++ app.py
@@ -1,48 +1,68 @@
 import streamlit as st
 import os
-from llama_index.core import (
-    StorageContext,
-    load_index_from_storage,
-    get_response_synthesizer,
-    Settings
-    )
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.core.retrievers import VectorIndexRetriever
 from llama_index.core.query_engine import RetrieverQueryEngine
 from llama_index.core.postprocessor import SimilarityPostprocessor
 from lib.run_query import load_query_engine
+from llama_index.core.response.notebook_utils import display_response
 import time
-#from openai import OpenAI
+
+from llama_index.core import (
+    StorageContext,
+    load_index_from_storage,
+    get_response_synthesizer,
+    Settings
+)
 
 
+def check_embeddings():
+    if not os.path.exists("./embedding_storage"):
+        st.warning("""Embeddings not found. Please follow the instructions in the README to generate embeddings.
+                   and make sure the embedding_storage is in the './embedding_storage' directory.""")
+        st.stop()
 
-st.title("Spotify Review Chatbot")
-if not os.path.exists("./embedding_storage"):
-    st.warning("""Embeddings not found. Please follow the instructions in the README to generate embeddings.
-               and make sure the embedding_storage is in the './embedding_storage' directory.""")
-    st.stop()
 
-with st.spinner("Calling Chatbot..."):
-    query_engine = load_query_engine(embedding_dir="./embedding_storage")
+def load_chatbot():
+
+    with st.spinner("Calling Chatbot..., this make take a while..."):
+        query_engine = load_query_engine(embedding_dir="./embedding_storage", load_custom_llm=False)
     time.sleep(2)  # Simulating a long-running process
-st.success("Chatbot is here!")
+    st.success("Chatbot is here!")
+    return query_engine
 
 
-# Initialize chat history
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+def initialize_chat_history():
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
 
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
-if prompt := st.chat_input("What is up?"):
-    with st.chat_message("user"):
-        st.markdown(prompt)
-    st.session_state.messages.append({"role": "User", "content": prompt})
 
-    response = query_engine.query(prompt)
-    st.session_state.messages.append({"role": "bot", "content": response})
-    with st.chat_message("Assistant"):
-        st.markdown(response)
+def process_user_input(query_engine):
+    if prompt := st.chat_input("What is up?"):
+        with st.chat_message("user"):
+            st.markdown(prompt)
+        st.session_state.messages.append({"role": "User", "content": prompt})
 
+        response = query_engine.query(prompt)
+        st.session_state.messages.append({"role": "bot", "content": response})
+        if response.response == "Empty Response":
+            response = "I'm sorry, I don't have an answer for that. Please ask me something else."
+        with st.chat_message("Assistant"):
+            st.markdown(response)
+
+
+def main():
+    st.title("Spotify Review Chatbot")
+    check_embeddings()
+    
+    query_engine = load_chatbot()
+    initialize_chat_history()
+    process_user_input(query_engine)
+
+
+if __name__ == "__main__":
+    main()
--- lib/run_query.py
+++ lib/run_query.py
@@ -5,11 +5,37 @@ from llama_index.core import (
     get_response_synthesizer,
     Settings
     )
+import torch
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.llms.huggingface import HuggingFaceLLM
 from llama_index.core.retrievers import VectorIndexRetriever
 from llama_index.core.query_engine import RetrieverQueryEngine
 from llama_index.core.postprocessor import SimilarityPostprocessor
+from llama_index.core.response_synthesizers import TreeSummarize
+from llama_index.core import PromptTemplate
+from llama_index.llms.openai import OpenAI
+
+from transformers import BitsAndBytesConfig
+from llama_index.core.prompts import PromptTemplate
+from llama_index.llms.huggingface import HuggingFaceLLM
+
+
+
 import argparse
+
+QA_PROMPT_TEMPLATE = (
+    """Context information is below.\n
+    You are a chatbot for our company spotify, your purpose is to answer based on context given\n
+    ---------------------\n
+    {context_str}\n"
+    ---------------------\n
+    only answer the query based on the context given\n
+    answer in detail based on the context given\n
+    if the query is not relevant to context or context is empty, answer as 'query is not relevan' \n
+    Query: {query_str}\n
+    Answer: """
+)
+
 
 
 def get_args():
@@ -18,8 +44,30 @@ def get_args():
     parser.add_argument("--prompt","-p", type=str, default="what is the common reasons people give low ratings, answer in list from the most common", help="Query to search")
     return parser.parse_args()
 
+def load_mistral():
+    quantization_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_compute_dtype=torch.float16,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_use_double_quant=True,
+    )
 
-def load_query_engine(embedding_dir):
+
+    llm = HuggingFaceLLM(
+        model_name="mistralai/Mistral-7B-Instruct-v0.1",
+        tokenizer_name="mistralai/Mistral-7B-Instruct-v0.1",
+        query_wrapper_prompt=PromptTemplate("<s>[INST] {query_str} [/INST] </s>\n"),
+        context_window=3900,
+        max_new_tokens=256,
+        model_kwargs={"quantization_config": quantization_config},
+        # tokenizer_kwargs={},
+        generate_kwargs={"temperature": 0.2, "top_k": 5, "do_sample":True},
+        device_map="cuda:0",
+    )
+    return llm
+
+
+def load_query_engine(embedding_dir, load_custom_llm=False):
     Settings.embed_model = HuggingFaceEmbedding(
         model_name="BAAI/bge-small-en-v1.5"
     )
@@ -29,13 +77,30 @@ def load_query_engine(embedding_dir):
         index=index,
         similarity_top_k=20,
     )
-    response_synthesizer = get_response_synthesizer()
+    if load_custom_llm: # still has bug
+        llm = load_mistral()
+        Settings.llm = llm
+        query_engine = index.as_query_engine(response_mode="compact")
 
-    query_engine = RetrieverQueryEngine(
-        retriever=retriever,
-        response_synthesizer=response_synthesizer,
-        node_postprocessors=[SimilarityPostprocessor(similarity_cutoff=0.5)],
-    )
+    else:
+        llm = OpenAI(model="gpt-3.5-turbo-0613")
+        Settings.llm = llm
+        qa_prompt = PromptTemplate(QA_PROMPT_TEMPLATE)
+        summarizer = get_response_synthesizer(
+            response_mode="compact",  
+            verbose=True, 
+            llm=llm, 
+            structured_answer_filtering=True,
+            text_qa_template=qa_prompt,
+
+            )
+        query_engine = RetrieverQueryEngine(
+            retriever=retriever,
+            response_synthesizer=summarizer,
+            node_postprocessors=[SimilarityPostprocessor(similarity_cutoff=0.1)],
+        )
+
+    
     return query_engine
 
 if __name__ == "__main__":
